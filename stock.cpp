cdc6961baf1ec5afc2d45be49ab38cdb4717f279
Patrick Kabuga <kabugap02@gmail.com>
2022-10-07 00:56:29 +0300

    Simple stocks assinment

---
+++ stock.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+class Stock
+{
+    private:
+        char name[20];
+        float itemPrice, totalPrice;
+        float quantity;
+
+    public:
+        void registerItem()
+        {
+            cout << "What is the name of the item? ";
+            cin >> name;
+
+            cout << "What is the quantity of the item? ";
+            cin >> quantity;
+
+            cout << "What is the price of one item? ";
+            cin >> itemPrice;
+
+            totalPrice = itemPrice * quantity;
+        }
+
+        void sale()
+        {
+            char responseName[20];
+            float responseQuant;
+            float totalSale;
+
+            // cout << "What would you like to buy? ";
+            
+            // for (int i = 0, n = strlen(responseName); i < n; i++)
+            // {
+            //     cin >> responseName[i];
+            // }
+
+            cout << "What quantity would you like to buy? ";
+            cin >> responseQuant;
+
+            // Updating the stock quantity
+            quantity += responseQuant;
+        }
+
+        void salesReport()
+        {
+            cout << "The current stock name is ";
+
+            for (int i = 0, n = strlen(name); i < n; i++)
+            {
+                cout << name[i];
+            }
+            cout << endl;
+
+            cout << "The quantity remaining is " << quantity << endl;
+        }
+};
+
+
+int main()
+{
+    Stock one;
+
+    one.registerItem();
+    one.sale();
+    one.salesReport();
+    return 0;
+}
