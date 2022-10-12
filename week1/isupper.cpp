c4f264abc79b0222366df06cb6f83ead58fab46a
Ebenezer <akhonyaebenezer@gmail.com>
2022-10-13 01:17:57 +0300

    Added isupper.cpp - a program that checks to see if athe character read from stdin is uppercase or not

---
+++ week1/isupper.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+/**
+    * main - check if a character is uppercase or not 
+    * return: (0) for success
+*/
+int main(void)
+{
+    char ch;
+
+    cout << "Enter a character to check: ";
+    cin >> ch;
+    /** check to see if the character is an alphabet first **/
+    if (ch >= 65 && ch <= 122) 
+    {
+        if ( ch >= 65 && ch <= 97)
+        {
+            cout << ch << " is Uppercase\n";
+        }
+        else
+        {
+            cout << ch << " is lowercase\n";
+        }
+        
+    }
+    else
+    {
+        cout << ch << "Is not even an alphabet, its impossible to determine its case\n";
+    }
+}
