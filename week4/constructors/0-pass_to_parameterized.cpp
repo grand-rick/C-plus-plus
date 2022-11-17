8dc50b31a6e98db27e8251491b9e6befd56d1dec
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-18 00:17:04 +0300

    Added 0-pass_to_parameterized.cpp

---
+++ week4/constructors/0-pass_to_parameterized.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class  Add
+{
+public:
+  int x,y;
+
+  Add(int v1, int v2)
+  {
+    x = v1;
+    y = v2;
+  }
+
+  int add(int a,int b)
+  {
+    return (a + b);
+  }
+
+};
+
+
+int main()
+{
+  int first, second;
+  cout << "What is the value of x\n";
+  cin >> first;
+
+  cout << "What is the value of y\n";
+  cin >> second;
+
+  Add a(first, second);
+
+  int sum = a.add(a.x, a.y);
+
+  cout << sum << "\n";
+
+  return (0);
+
+}
